5adc5e0cfc35c0a88b3c2d2f1911b7e4caae435a
Kevin Cheddar <kevincheddar@gmail.com>
2018-12-14 21:41:07 -0500

    Fixed Skip Title Button

--- client/src/Mobile/MobileView.jsx
+++ client/src/Mobile/MobileView.jsx
@@ -1,127 +1,241 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import MobileSlides from './MobileSlides';
-import MobileItemList from './MobileItemList';
-import MobileDisplay from './MobileDisplay';
-import MobileListChanger from './MobileListChanger';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import MobileSlides from "./MobileSlides";
+import MobileItemList from "./MobileItemList";
+import MobileDisplay from "./MobileDisplay";
+import MobileListChanger from "./MobileListChanger";
 // import MobileArrangeLyrics from './MobileArrangeLyrics';
 
-import on from '../assets/on.png';
-import off from '../assets/off.png';
-import connected from '../assets/connected.png';
+import on from "../assets/on.png";
+import off from "../assets/off.png";
+import connected from "../assets/connected.png";
 
-export default class MobileView extends Component{
-
-  constructor(){
+export default class MobileView extends Component {
+  constructor() {
     super();
     this.state = {
       itemSelectOpen: false,
       menuOpen: false,
       arrangeLyricsOpen: false
-    }
+    };
   }
 
   openArrangeLyrics = () => {
-    this.setState({arrangeLyricsOpen: true})
-  }
+    this.setState({ arrangeLyricsOpen: true });
+  };
   closeArrangeLyrics = () => {
-    this.setState({arrangeLyricsOpen: false})
-  }
+    this.setState({ arrangeLyricsOpen: false });
+  };
 
   openItemSelect = () => {
-    this.setState({itemSelectOpen: true})
-  }
+    this.setState({ itemSelectOpen: true });
+  };
   closeItemSelect = () => {
-    this.setState({itemSelectOpen: false})
-  }
+    this.setState({ itemSelectOpen: false });
+  };
   openMenu = () => {
-    this.setState({menuOpen: true})
-  }
+    this.setState({ menuOpen: true });
+  };
   closeMenu = () => {
-    if(this.state.menuOpen === true)
-      this.setState({menuOpen: false})
-  }
+    if (this.state.menuOpen === true) this.setState({ menuOpen: false });
+  };
 
   render() {
+    let {
+      setItemIndex,
+      setWordIndex,
+      updateItem,
+      toggleFreeze,
+      selectItemList,
+      logout
+    } = this.props.parent; //connectToReceiver,
+    let {
+      wordIndex,
+      itemList,
+      item,
+      backgrounds,
+      itemIndex,
+      freeze,
+      itemLists,
+      selectedItemList,
+      isLoggedIn,
+      isSender,
+      boxIndex
+    } = this.props.parent.state;
 
-    let {setItemIndex, setWordIndex, updateItem, toggleFreeze, selectItemList,
-         logout} = this.props.parent; //connectToReceiver,
-    let {wordIndex, itemList, item, backgrounds, itemIndex, freeze, itemLists,
-       selectedItemList, isLoggedIn, isSender, boxIndex} = this.props.parent.state;
-
-    let buttonLoggedIn = {margin:'auto', width: '25vw', minWidth:'18vw', backgroundColor: '#383838',
-    color: 'white', height: '4.5vh'}
+    let buttonLoggedIn = {
+      margin: "auto",
+      width: "25vw",
+      minWidth: "18vw",
+      backgroundColor: "#383838",
+      color: "white",
+      height: "4.5vh"
+    };
     let menuItem = {
-      display:'inline-block', height: '5vh', width:'95%', backgroundColor:'#06d1d1', margin: '10% 2.5%',
-      fontSize: "calc(8px + 0.75vw)", color: 'black'
-    }
-    let connectedIcon = {marginLeft:'0.5vw', width:'5vw', height:'3.85vw', margin: 'auto', display:'block'}
-    let menuStyle = {backgroundColor:'#383838', position:'absolute', width:'22vw', top:'5.5vh', left:'4.25vw',
-    zIndex: 3}
+      display: "inline-block",
+      height: "5vh",
+      width: "95%",
+      backgroundColor: "#06d1d1",
+      margin: "10% 2.5%",
+      fontSize: "calc(8px + 0.75vw)",
+      color: "black"
+    };
+    let connectedIcon = {
+      marginLeft: "0.5vw",
+      width: "5vw",
+      height: "3.85vw",
+      margin: "auto",
+      display: "block"
+    };
+    let menuStyle = {
+      backgroundColor: "#383838",
+      position: "absolute",
+      width: "22vw",
+      top: "5.5vh",
+      left: "4.25vw",
+      zIndex: 5
+    };
     return (
-      <div style={{width:'100%', overflow:'hidden', height:'100vh', backgroundColor:'#383838',
-        position: 'fixed'}} onClick={this.closeMenu}>
-        <div style={{width:'100%', height:'5vh', paddingTop:'0.5vh', paddingBottom:'0.5vh',
-          backgroundColor:'#06d1d1', display:'flex'}}>
-          <button style={buttonLoggedIn}
-            onClick={this.openMenu}>Menu</button>
-          <div style={this.state.menuOpen ? menuStyle : {display:'none'}}>
-              <button style={menuItem} onClick={() => this.props.history.push("/")}>Home</button>
-                {/*{(item.type === 'song') && <button style={menuItem}
+      <div
+        style={{
+          width: "100%",
+          overflow: "hidden",
+          height: "100vh",
+          backgroundColor: "#383838",
+          position: "fixed"
+        }}
+        onClick={this.closeMenu}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "5vh",
+            paddingTop: "0.5vh",
+            paddingBottom: "0.5vh",
+            backgroundColor: "#06d1d1",
+            display: "flex"
+          }}
+        >
+          <button style={buttonLoggedIn} onClick={this.openMenu}>
+            Menu
+          </button>
+          <div style={this.state.menuOpen ? menuStyle : { display: "none" }}>
+            <button
+              style={menuItem}
+              onClick={() => this.props.history.push("/")}
+            >
+              Home
+            </button>
+            {/*{(item.type === 'song') && <button style={menuItem}
                 onClick={this.openArrangeLyrics}>Arrange Lyrics</button>}
                 <button style={menuItem} onClick={connectToReceiver}> Connect To Receiver </button>*/}
-                {isSender && <img style={connectedIcon}
-                   alt="connected" src={connected}
-                  />}
-              {!isLoggedIn &&<Link to="/login"><button style={menuItem}>Login</button></Link>}
-              {isLoggedIn && <button style={menuItem} onClick={logout}>Logout</button>}
-            </div>
-          <button style={buttonLoggedIn}
-            onClick={this.openItemSelect}
-            >Change Item</button>
-          {freeze && <div style={{display:'flex', margin:'auto'}}>
-              <button style={buttonLoggedIn} onClick={toggleFreeze}>Unfreeze</button>
-              <img style={{width: '12vw', height: '5.52vw', margin: '1vh 1vw'}}
-                 alt="off" src={off}/>
-              </div>
-            }
-            {!freeze && <div style={{display:'flex', margin:'auto'}}>
-            <button style={buttonLoggedIn} onClick={toggleFreeze}>Freeze</button>
-              <img style={{width: '12vw', height: '5.52vw', margin: '1vh 1vw'}}
-                 alt="on" src={on}/>
-              </div>
-            }
-        </div>
-        <div style={{display:'block', fontSize:'calc(10px + 1vmax)'}}>
-          <div style={{width:'100vw', height: '56.25vw'}}>
-            <MobileDisplay wordIndex={wordIndex} backgrounds={backgrounds}
-              item={item} updateItem={updateItem}/>
+            {isSender && (
+              <img style={connectedIcon} alt="connected" src={connected} />
+            )}
+            {!isLoggedIn && (
+              <Link to="/login">
+                <button style={menuItem}>Login</button>
+              </Link>
+            )}
+            {isLoggedIn && (
+              <button style={menuItem} onClick={logout}>
+                Logout
+              </button>
+            )}
           </div>
-          <div style={{paddingTop:'1.5%', maxWidth:'100%'}}>
-            <MobileSlides setWordIndex={setWordIndex} wordIndex={wordIndex}
-              item={item} updateItem={updateItem} boxIndex={boxIndex}
-              backgrounds={backgrounds}
+          <button style={buttonLoggedIn} onClick={this.openItemSelect}>
+            Change Item
+          </button>
+          {freeze && (
+            <div style={{ display: "flex", margin: "auto" }}>
+              <button style={buttonLoggedIn} onClick={toggleFreeze}>
+                Unfreeze
+              </button>
+              <img
+                style={{ width: "12vw", height: "5.52vw", margin: "1vh 1vw" }}
+                alt="off"
+                src={off}
               />
+            </div>
+          )}
+          {!freeze && (
+            <div style={{ display: "flex", margin: "auto" }}>
+              <button style={buttonLoggedIn} onClick={toggleFreeze}>
+                Freeze
+              </button>
+              <img
+                style={{ width: "12vw", height: "5.52vw", margin: "1vh 1vw" }}
+                alt="on"
+                src={on}
+              />
+            </div>
+          )}
+        </div>
+        <div style={{ display: "block", fontSize: "calc(10px + 1vmax)" }}>
+          <div style={{ width: "100vw", height: "56.25vw" }}>
+            <MobileDisplay
+              wordIndex={wordIndex}
+              backgrounds={backgrounds}
+              item={item}
+              updateItem={updateItem}
+            />
+          </div>
+          <div style={{ paddingTop: "1.5%", maxWidth: "100%" }}>
+            <MobileSlides
+              setWordIndex={setWordIndex}
+              wordIndex={wordIndex}
+              item={item}
+              updateItem={updateItem}
+              boxIndex={boxIndex}
+              backgrounds={backgrounds}
+            />
           </div>
         </div>
-        {this.state.itemSelectOpen &&
-          <div style={{position:'fixed', top:0, left:0, height:'100vh',
-             zIndex: 2, backgroundColor:'rgba(62, 64, 66, 0.5)', width:'100vw'}}>
-            <div style={{position:'fixed', display:'flex', top:'2vmax', left:'13vw',
-               zIndex: 3, backgroundColor:'#383838', width:'70vw', maxWidth:'100%',
-             color: 'white'}}>
-             <MobileListChanger selectedItemList={selectedItemList}
-               selectItemList={selectItemList} itemLists={itemLists}/>
-              <MobileItemList setItemIndex={setItemIndex} setWordIndex={setWordIndex}
-                itemList={itemList} backgrounds={backgrounds} updateItem={updateItem}
-                 itemIndex={itemIndex} close={this.closeItemSelect}
-                />
+        {this.state.itemSelectOpen && (
+          <div
+            style={{
+              position: "fixed",
+              top: 0,
+              left: 0,
+              height: "100vh",
+              zIndex: 4,
+              backgroundColor: "rgba(62, 64, 66, 0.5)",
+              width: "100vw"
+            }}
+          >
+            <div
+              style={{
+                position: "fixed",
+                display: "flex",
+                top: "2vmax",
+                left: "13vw",
+                zIndex: 5,
+                backgroundColor: "#383838",
+                width: "70vw",
+                maxWidth: "100%",
+                color: "white"
+              }}
+            >
+              <MobileListChanger
+                selectedItemList={selectedItemList}
+                selectItemList={selectItemList}
+                itemLists={itemLists}
+              />
+              <MobileItemList
+                setItemIndex={setItemIndex}
+                setWordIndex={setWordIndex}
+                itemList={itemList}
+                backgrounds={backgrounds}
+                updateItem={updateItem}
+                itemIndex={itemIndex}
+                close={this.closeItemSelect}
+              />
             </div>
-        </div>}
+          </div>
+        )}
       </div>
-    )
+    );
   }
-
 }
 
 // {this.state.arrangeLyricsOpen &&
--- client/src/ToolbarElements/FormatEditor.jsx
+++ client/src/ToolbarElements/FormatEditor.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import {ChromePicker} from 'react-color';
-import fsUP from '../assets/fontSizeUP.png';
-import fsDOWN from '../assets/fontSizeDOWN.png';
-import cPicker from '../assets/color-picker.png';
-import cPickerClose from '../assets/color-picker-close.png';
-import brightness_img from '../assets/brightness.png';
-import skipTitle from '../assets/skipTitle.png';
-import skipTitleOff from '../assets/skipTitleOff.png'
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import React from "react";
+import { ChromePicker } from "react-color";
+import fsUP from "../assets/fontSizeUP.png";
+import fsDOWN from "../assets/fontSizeDOWN.png";
+import cPicker from "../assets/color-picker.png";
+import cPickerClose from "../assets/color-picker-close.png";
+import brightness_img from "../assets/brightness.png";
+import skipTitle from "../assets/skipTitle.png";
+import skipTitleOff from "../assets/skipTitleOff.png";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
-class FormatEditor extends React.Component{
-
-  constructor(){
+class FormatEditor extends React.Component {
+  constructor() {
     super();
     this.state = {
       cPickerOpen: false,
@@ -20,37 +19,37 @@ class FormatEditor extends React.Component{
       fontSize: 2,
       updating: false,
       brightness: 100
-    }
+    };
 
-    this.throttle = null
-
+    this.throttle = null;
   }
 
-  componentDidUpdate(prevProps, prevState){
+  componentDidUpdate(prevProps, prevState) {
+    let { item, wordIndex, boxIndex } = this.props;
 
-    let{item, wordIndex, boxIndex} = this.props;
-
-    if((wordIndex !== prevProps.wordIndex) || (item._id !== prevProps.item._id) ||
-        boxIndex!== prevProps.boxIndex){
+    if (
+      wordIndex !== prevProps.wordIndex ||
+      item._id !== prevProps.item._id ||
+      boxIndex !== prevProps.boxIndex
+    ) {
       let slides;
-      if (item.type === 'song')
+      if (item.type === "song")
         slides = item.arrangements[item.selectedArrangement].slides || null;
-      else
-        slides = item.slides || null;
+      else slides = item.slides || null;
 
       let slide = slides ? slides[wordIndex] : null;
-      if(!slide)
-        return;
-      if(slide.boxes[0].brightness !== undefined)
-        this.setState({brightness: slide.boxes[0].brightness})
-      else{
-        this.setState({brightness: 100})
+      if (!slide) return;
+      if (slide.boxes[0].brightness !== undefined)
+        this.setState({ brightness: slide.boxes[0].brightness });
+      else {
+        this.setState({ brightness: 100 });
       }
 
-      if(!slide.boxes[boxIndex].fontSize)
-        return
-      this.setState({fontSize: slide.boxes[boxIndex].fontSize})
-      let stringToRGB = slide.boxes[boxIndex].fontColor.replace(/[^\d,]/g, '').split(',');
+      if (!slide.boxes[boxIndex].fontSize) return;
+      this.setState({ fontSize: slide.boxes[boxIndex].fontSize });
+      let stringToRGB = slide.boxes[boxIndex].fontColor
+        .replace(/[^\d,]/g, "")
+        .split(",");
       this.setState({
         color: {
           r: stringToRGB[0],
@@ -58,167 +57,265 @@ class FormatEditor extends React.Component{
           b: stringToRGB[2],
           a: stringToRGB[3]
         }
-      })
+      });
     }
   }
 
   openColors = () => {
-    this.setState({cPickerOpen: true})
-  }
+    this.setState({ cPickerOpen: true });
+  };
 
   closeColors = () => {
     this.setState({
       cPickerOpen: false
-    })
-  }
+    });
+  };
 
-  changeBrightness = (level) => {
-    this.setState({brightness: level})
-  }
+  changeBrightness = level => {
+    this.setState({ brightness: level });
+  };
 
-  colorChange = (event) => {
-    this.setState({color: event.rgb})
-    this.props.updateFontColor(event.rgb)
-  }
+  colorChange = event => {
+    this.setState({ color: event.rgb });
+    this.props.updateFontColor(event.rgb);
+  };
 
-  updateFont = (fontSize) => {
-    if(fontSize > 10){
-      fontSize = 10
+  updateFont = fontSize => {
+    if (fontSize > 10) {
+      fontSize = 10;
+    } else if (fontSize < 0.1) {
+      fontSize = 0.1;
     }
-    else if(fontSize < 0.1){
-      fontSize = 0.1
-    }
-    fontSize = Math.round(fontSize*10)/10;
-    this.setState({fontSize: fontSize, updating: false})
-    this.props.updateFontSize(fontSize)
-  }
+    fontSize = Math.round(fontSize * 10) / 10;
+    this.setState({ fontSize: fontSize, updating: false });
+    this.props.updateFontSize(fontSize);
+  };
 
   fontSizeUP = () => {
-    let {fontSize} = this.state;
-    this.updateFont(fontSize+0.1)
-  }
+    let { fontSize } = this.state;
+    this.updateFont(fontSize + 0.1);
+  };
 
   fontSizeDOWN = () => {
-    let {fontSize} = this.state;
-    this.updateFont(fontSize-0.1)
-  }
+    let { fontSize } = this.state;
+    this.updateFont(fontSize - 0.1);
+  };
 
-  fontSizeChange = (event) => {
+  fontSizeChange = event => {
     let val;
-    let {updating} = this.state;
+    let { updating } = this.state;
     let that = this;
-    if(event.target.value.length === 0){
-      this.setState({fontSize: ''})
+    if (event.target.value.length === 0) {
+      this.setState({ fontSize: "" });
       return;
     }
     val = parseInt(event.target.value, 10);
-    val/=10;
+    val /= 10;
 
-    if(val > 10)
-      val = 10
+    if (val > 10) val = 10;
 
-    clearTimeout(this.throttle)
-    if(updating){
-      this.throttle = setTimeout(function(){
+    clearTimeout(this.throttle);
+    if (updating) {
+      this.throttle = setTimeout(function() {
         let fontSize = that.state.fontSize;
-        that.updateFont(fontSize)
-      }, 100)
+        that.updateFont(fontSize);
+      }, 100);
+    } else {
+      this.setState({ updating: true });
+      this.updateFont(val);
     }
-    else{
-      this.setState({updating: true})
-      this.updateFont(val)
-    }
-
-  }
+  };
 
   updateSkipTitle = () => {
-    if(this.props.item.skipTitle)
-      this.props.updateSkipTitle(false)
-    else
-      this.props.updateSkipTitle(true)
-  }
+    if (this.props.item.skipTitle) this.props.updateSkipTitle(false);
+    else this.props.updateSkipTitle(true);
+  };
 
   updateNextOnFinish = () => {
-    if(this.props.item.nextOnFinish)
-      this.props.updateNextOnFinish(false)
-    else
-      this.props.updateNextOnFinish(true)
-  }
+    if (this.props.item.nextOnFinish) this.props.updateNextOnFinish(false);
+    else this.props.updateNextOnFinish(true);
+  };
 
   render() {
-
-    let {cPickerOpen, fontSize, brightness} = this.state;
-    let sliderStyle = {width: '5vw', margin: '.5vw 1vw'}
-    let {item} = this.props;
+    let { cPickerOpen, fontSize, brightness } = this.state;
+    let sliderStyle = { width: "5vw", margin: ".5vw 1vw" };
+    let { item } = this.props;
     let showSkipTitle = false;
-    if(item.type === 'song')
-      if(item.arrangements[item.selectedArrangement].slides.length > 1)
+    if (item.type === "song")
+      if (item.arrangements[item.selectedArrangement].slides.length > 1)
         showSkipTitle = true;
 
     return (
       <div>
-        <div style={{display:'flex'}}>
-            <img className='imgButton' style={!cPickerOpen ? {marginRight:'1vw', fontSize: "calc(8px + 0.4vw)", width:'1.5vw', height: '1.5vw',} : {display:'none'}}
-              alt="cPicker" src={cPicker}
-              onClick={this.openColors}/>
-            <img className='imgButton' style={!cPickerOpen ? {display:'none'} : {marginRight:'1vw', fontSize: "calc(8px + 0.4vw)", width:'1.5vw', height: '1.5vw'} }
-              alt="cPickerClose" src={cPickerClose}
-              onClick={this.closeColors}/>
-            {cPickerOpen &&<div style={{position:'absolute', zIndex:5, top: '5vh', backgroundColor:'#EEE', padding:'5px'}}>
-                <ChromePicker color={this.state.color} onChange={this.colorChange}/>
-            </div>}
-           <input style={{width: '1.25vw', height: '1.25vw', textAlign: 'center',
-             marginRight:'1vw', fontSize:"calc(7px + 0.25vw)"}}
-             value={String(fontSize*10)} onChange={this.fontSizeChange}/>
-           <img className='imgButton' style={{width:'1.25vw', height: '1.25vw', marginRight:'1vw'}}
-              onClick={this.fontSizeUP}
-              alt="fsUP" src={fsUP}
-            />
-            <img className='imgButton' style={{width:'1.25vw', height: '1.25vw', marginRight:'1vw'}}
-                onClick={this.fontSizeDOWN}
-                alt="fsDOWN" src={fsDOWN}
-            />
-            <img style={{marginTop:'.25vw', width:'1.5vw', height:'1.5vw'}}
-                alt="brightness" src={brightness_img}
+        <div style={{ display: "flex" }}>
+          <img
+            className="imgButton"
+            style={
+              !cPickerOpen
+                ? {
+                    marginRight: "1vw",
+                    fontSize: "calc(8px + 0.4vw)",
+                    width: "1.5vw",
+                    height: "1.5vw"
+                  }
+                : { display: "none" }
+            }
+            alt="cPicker"
+            src={cPicker}
+            onClick={this.openColors}
+          />
+          <img
+            className="imgButton"
+            style={
+              !cPickerOpen
+                ? { display: "none" }
+                : {
+                    marginRight: "1vw",
+                    fontSize: "calc(8px + 0.4vw)",
+                    width: "1.5vw",
+                    height: "1.5vw"
+                  }
+            }
+            alt="cPickerClose"
+            src={cPickerClose}
+            onClick={this.closeColors}
+          />
+          {cPickerOpen && (
+            <div
+              style={{
+                position: "absolute",
+                zIndex: 5,
+                top: "5vh",
+                backgroundColor: "#EEE",
+                padding: "5px"
+              }}
+            >
+              <ChromePicker
+                color={this.state.color}
+                onChange={this.colorChange}
+              />
+            </div>
+          )}
+          <input
+            style={{
+              width: "1.25vw",
+              height: "1.25vw",
+              textAlign: "center",
+              marginRight: "1vw",
+              fontSize: "calc(7px + 0.25vw)"
+            }}
+            value={String(fontSize * 10)}
+            onChange={this.fontSizeChange}
+          />
+          <img
+            className="imgButton"
+            style={{ width: "1.25vw", height: "1.25vw", marginRight: "1vw" }}
+            onClick={this.fontSizeUP}
+            alt="fsUP"
+            src={fsUP}
+          />
+          <img
+            className="imgButton"
+            style={{ width: "1.25vw", height: "1.25vw", marginRight: "1vw" }}
+            onClick={this.fontSizeDOWN}
+            alt="fsDOWN"
+            src={fsDOWN}
+          />
+          <img
+            style={{ marginTop: ".25vw", width: "1.5vw", height: "1.5vw" }}
+            alt="brightness"
+            src={brightness_img}
+          />
+          <Slider
+            style={sliderStyle}
+            min={1}
+            value={brightness}
+            onChange={this.changeBrightness}
+            onAfterChange={() => this.props.updateBrightness(brightness)}
+          />
+        </div>
+        <div style={{ display: "flex" }}>
+          {showSkipTitle && (
+            <div
+              onClick={this.updateSkipTitle}
+              className="imgButton"
+              style={{
+                fontSize: "calc(5px + 0.35vw)",
+                height: "5vh",
+                marginRight: "0.5vw"
+              }}
+            >
+              {item.skipTitle && (
+                <img
+                  style={{
+                    display: "block",
+                    width: "1.25vw",
+                    height: "1.25vw",
+                    margin: "auto",
+                    padding: "0.25vh 0.25vw"
+                  }}
+                  alt="skipTitle"
+                  src={skipTitle}
                 />
-            <Slider style={sliderStyle} min={1} value={brightness} onChange={this.changeBrightness}
-              onAfterChange={() => this.props.updateBrightness(brightness)}/>
-          </div>
-          <div style={{display: 'flex'}}>
-            {(showSkipTitle) &&<div onClick={this.updateNextOnFinish} className='imgButton'
-               style={{fontSize: "calc(5px + 0.35vw)", height: '5vh', marginRight:'0.5vw'}}>
-               {item.skipTitle && <img
-                style={{display:'block', width:'1.25vw', height:'1.25vw', margin: 'auto',
-                  padding: '0.25vh 0.25vw'}}
-                 alt="skipTitle" src={skipTitle}
-                 />}
-               {!item.skipTitle && <img
-                style={{display:'block', width:'1.25vw', height:'1.25vw', margin: 'auto',
-                  padding: '0.25vh 0.25vw'}}
-                 alt="skipTitleOff" src={skipTitleOff}
-                 />}
-               <div>Skip Title</div>
-            </div>}
-            {(item.type === 'timer') &&<div onClick={this.updateNextOnFinish} className='imgButton'
-               style={{fontSize: "calc(5px + 0.35vw)", height: '5vh', marginRight:'0.5vw'}}>
-               {item.nextOnFinish && <img
-                style={{display:'block', width:'1.25vw', height:'1.25vw', margin: 'auto',
-                  padding: '0.25vh 0.25vw'}}
-                 alt="skipTitle" src={skipTitle}
-                 />}
-               {!item.nextOnFinish && <img
-                style={{display:'block', width:'1.25vw', height:'1.25vw', margin: 'auto',
-                  padding: '0.25vh 0.25vw'}}
-                 alt="skipTitleOff" src={skipTitleOff}
-                 />}
-               <div>Next On Finish</div>
-            </div>}
-
-          </div>
+              )}
+              {!item.skipTitle && (
+                <img
+                  style={{
+                    display: "block",
+                    width: "1.25vw",
+                    height: "1.25vw",
+                    margin: "auto",
+                    padding: "0.25vh 0.25vw"
+                  }}
+                  alt="skipTitleOff"
+                  src={skipTitleOff}
+                />
+              )}
+              <div>Skip Title</div>
+            </div>
+          )}
+          {item.type === "timer" && (
+            <div
+              onClick={this.updateNextOnFinish}
+              className="imgButton"
+              style={{
+                fontSize: "calc(5px + 0.35vw)",
+                height: "5vh",
+                marginRight: "0.5vw"
+              }}
+            >
+              {item.nextOnFinish && (
+                <img
+                  style={{
+                    display: "block",
+                    width: "1.25vw",
+                    height: "1.25vw",
+                    margin: "auto",
+                    padding: "0.25vh 0.25vw"
+                  }}
+                  alt="skipTitle"
+                  src={skipTitle}
+                />
+              )}
+              {!item.nextOnFinish && (
+                <img
+                  style={{
+                    display: "block",
+                    width: "1.25vw",
+                    height: "1.25vw",
+                    margin: "auto",
+                    padding: "0.25vh 0.25vw"
+                  }}
+                  alt="skipTitleOff"
+                  src={skipTitleOff}
+                />
+              )}
+              <div>Next On Finish</div>
+            </div>
+          )}
+        </div>
       </div>
-
-    )
+    );
   }
-
 }
 export default FormatEditor;
