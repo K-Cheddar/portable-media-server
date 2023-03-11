154deb0f5b1a42371aec0e50e7c53239ca7efaa8
Kevin Cheddar <kevincheddar@gmail.com>
2023-03-10 21:23:46 -0500

    Test updating these files

---
+++ client/build/precache-manifest.4aa5c8ce2d5d825939dc76d9f78e9bc8.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "2daf77becd5d536bc5fe",
+    "url": "/static/css/main.61c2e5cf.chunk.css"
+  },
+  {
+    "revision": "2daf77becd5d536bc5fe",
+    "url": "/static/js/main.ce4d3b6d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da8e7ef59de9f8a1baca",
+    "url": "/static/css/2.25755c8d.chunk.css"
+  },
+  {
+    "revision": "da8e7ef59de9f8a1baca",
+    "url": "/static/js/2.06db26a0.chunk.js"
+  },
+  {
+    "revision": "da6302ed6edde1016d203e196c318b82",
+    "url": "/static/media/deleteX.da6302ed.png"
+  },
+  {
+    "revision": "264eb30daa2a2dc3d5f30ed117551a1e",
+    "url": "/static/media/addItem.264eb30d.png"
+  },
+  {
+    "revision": "fe6198ec7a183d23e6ca1186aa80f146",
+    "url": "/static/media/closeIcon.fe6198ec.png"
+  },
+  {
+    "revision": "0dd83694c7da1c2e85382ec7561e7112",
+    "url": "/static/media/edit.0dd83694.png"
+  },
+  {
+    "revision": "82b554ba084d25a16f598af6fa4ab7ff",
+    "url": "/static/media/on.82b554ba.png"
+  },
+  {
+    "revision": "01b2530c1c52510b8c0d51ea026db970",
+    "url": "/static/media/off.01b2530c.png"
+  },
+  {
+    "revision": "95f3183d87b79f8e9241c0c172149903",
+    "url": "/static/media/check.95f3183d.png"
+  },
+  {
+    "revision": "3f3181aed1da316dc2555a13a2f3f174",
+    "url": "/static/media/cancel-icon.3f3181ae.png"
+  },
+  {
+    "revision": "31a866aa9b276f83bfd99123c3baeb81",
+    "url": "/static/media/duplicate.31a866aa.png"
+  },
+  {
+    "revision": "f8d3d91cbaa95349d56058762e93c5c1",
+    "url": "/static/media/zoomIn.f8d3d91c.png"
+  },
+  {
+    "revision": "a729a116f81cfcdcde62399b860b4fa7",
+    "url": "/static/media/zoomOut.a729a116.png"
+  },
+  {
+    "revision": "a2d553d60c101cf46989a68741cd4331",
+    "url": "/static/media/new-button.a2d553d6.png"
+  },
+  {
+    "revision": "13d2cf9ea82d8482efcb987caede42a8",
+    "url": "/static/media/brightness.13d2cf9e.png"
+  },
+  {
+    "revision": "2dcd6e895bb65923085a71a3c05c2e5a",
+    "url": "/static/media/bookmark.2dcd6e89.png"
+  },
+  {
+    "revision": "824cba3cb4526dc1c1df6ce01b3d6b05",
+    "url": "/static/media/lock.824cba3c.png"
+  },
+  {
+    "revision": "5e8893b4e0834c56ee5ace7d7f60adf8",
+    "url": "/static/media/unlock.5e8893b4.png"
+  },
+  {
+    "revision": "7dfc39db35f6834d53f5b3d2f0deb759",
+    "url": "/static/media/expandDown.7dfc39db.png"
+  },
+  {
+    "revision": "6dfb8b2e4af25ed40bea088c025f9ef9",
+    "url": "/static/media/collapseUp.6dfb8b2e.png"
+  },
+  {
+    "revision": "e09f56514ef0e26e05d69557215daa09",
+    "url": "/static/media/upload.e09f5651.png"
+  },
+  {
+    "revision": "ccdf909e294e8e5df940eb50b7da5f79",
+    "url": "/static/media/left-arrow.ccdf909e.png"
+  },
+  {
+    "revision": "ce180c74ca569fe59f58c2645c7ef94d",
+    "url": "/static/media/right-arrow.ce180c74.png"
+  },
+  {
+    "revision": "26ce9441f015f9de3af7c3f87c06d42d",
+    "url": "/static/media/fontSizeUP.26ce9441.png"
+  },
+  {
+    "revision": "81c507c45927e4d49c8ccb84f55bdbb0",
+    "url": "/static/media/fontSizeDOWN.81c507c4.png"
+  },
+  {
+    "revision": "95b9f00796dedc6c0062ebd0b2cad97f",
+    "url": "/static/media/color-picker.95b9f007.png"
+  },
+  {
+    "revision": "24c77d9726a879ed381326bc8d7f4f85",
+    "url": "/static/media/color-picker-close.24c77d97.png"
+  },
+  {
+    "revision": "d7a46a1699b84681c21581a6799a12f9",
+    "url": "/static/media/playVerse.d7a46a16.png"
+  },
+  {
+    "revision": "d276bd03dd5425483b7e2ea042df3060",
+    "url": "/static/media/open.d276bd03.png"
+  },
+  {
+    "revision": "d3aec49d8736978c8b340f8dbc1c69ca",
+    "url": "/static/media/bibleIcon.d3aec49d.png"
+  },
+  {
+    "revision": "3093c1dced91aeca92bd18172bef54ce",
+    "url": "/static/media/songIcon.3093c1dc.png"
+  },
+  {
+    "revision": "7c3d41671816c867398afc53d95ca5ef",
+    "url": "/static/media/allItemsIcon.7c3d4167.png"
+  },
+  {
+    "revision": "2a5d6fbd5d9c55d0c231b20b7df51134",
+    "url": "/static/media/timerIcon.2a5d6fbd.png"
+  },
+  {
+    "revision": "0df8f53f9538e71bff7ea506fde09f86",
+    "url": "/static/media/live_streaming_icon.0df8f53f.png"
+  },
+  {
+    "revision": "30ad16e3ea10572fbade4931a138c122",
+    "url": "/static/media/announcementsIcon.30ad16e3.png"
+  },
+  {
+    "revision": "121db4c8d864a13cc4fef58e248df60b",
+    "url": "/static/media/undo.121db4c8.png"
+  },
+  {
+    "revision": "d97107449395601222d3bc8ab88c15d5",
+    "url": "/static/media/redo.d9710744.png"
+  },
+  {
+    "revision": "868178b3c262354fd915568567351866",
+    "url": "/index.html"
+  }
+];
